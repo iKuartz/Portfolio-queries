7c15b0d55602a544cec6169551cf417d328ee94d
Ivan Silva <ivankuartz@hotmail.com>
2022-01-25 19:14:55 -0300

    HTML template added to js code

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     title: "Multi-Post Stories",
-    company: "CANPOY",
+    company: "CANOPY",
     position: "Back End Dev",
     date: "2015",
     desc: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
@@ -23,7 +23,7 @@ const projects = [
   },
   {
     title: "Multi-Post Stories",
-    company: "CANPOY",
+    company: "CANOPY",
     position: "Back End Dev",
     date: "2015",
     desc: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
@@ -56,35 +56,60 @@ projects.map((project) => {
 
 function generateHTML(details) {
   let template = `
-      <div class="works__single">
-        <div class="works__single__img_container">
-          <img class="works__single__img" src="${details.imageUrl}" alt="${details.title}" />
-        </div>
-      
-        <div class="works__single__holder">
-          <h2 class="works__single__title">${details.title}</h2>
-        
-          <div class="works__single__details">
-            <p class="works__single__details__single works__single__details__single--dark">
-              ${details.company}
-            </p>
-            <div class="dot"></div>
-            <p class="works__single__details__single works__single__details__single--light">
-              ${details.position}
-            </p>
-            <div class="dot"></div>
-            <p class="works__single__details__single works__single__details__single--light">
-              ${details.date}
-            </p>
-          </div>
-        
-          <p>
-          ${details.desc}
-          </p>
-        
-          <div class="works__single__tags">
-          </div>
-          <button id="${details.id}" class="button modal-open" data-modal="modal">See Project</button>
+      <div class="work">
+				<img
+					src="${details.imageUrl}"
+					alt="${details.title} screenshot"
+					class="work1-pic"
+				/>
+				<div class="mob-data">
+					<h2 class="work-name">${details.title}</h2>
+					<div class="work-data-tonic">
+						<strong class="company"> ${details.company} </strong>
+						<img src="./images/Counter.png" class="counter" alt="counter" />
+						<strong class="role">${details.position}</strong>
+						<img src="./images/Counter.png" class="counter" alt="counter" />
+						<strong class="year">${details.date}</strong>
+					</div>
+					<p class="paragraph-t">
+						${details.desc}
+					</p>
+					<div class="tags">
+						<img src="./images/html.png" alt="html tag" class="work-tags" />
+						<img src="./images/css.png" alt="css tag" class="work-tags" />
+						<img src="./images/javascript.png" alt="javascript tag" class="work-tags" />
+					</div>
+					<div>
+						<button id="${details.id}" class="btn-input" type="submit">Get In Touch</button>
+					</div>
+				</div>
+				<div class="work1-dsk"><img src="${details.imageUrl}" alt="${details.title} screenshot"/></div>
+				<div class="work1-dsk-txt">
+				<h3 class="work-name">${details.title}</h3>
+					<div class="work-data-tonic">
+					<strong class="company"> ${details.company} </strong>
+					<img src="./images/Counter.png" class="counter" alt="counter" />
+					<strong class="role">${details.position}</strong>
+					<img src="./images/Counter.png" class="counter" alt="counter" />
+					<strong class="year">${details.date}</strong>
+					</div>
+				<p class="paragraph-t">
+					${details.desc}
+				</p>
+					<div class="tags">
+					<img src="./images/html.png" alt="html tag" class="work-tags" />
+					<img src="./images/css.png" alt="css tag" class="work-tags" />
+					<img
+						src="./images/javascript.png"
+						alt="javascript tag"
+						class="work-tags"
+					/>
+					</div>
+					<div class="see-project">
+					<button id="${details.id}" class="btn-input" type="submit">Get In Touch</button>
+					</div>
+				</div>
+			</div>
 
 
           <div class="modal" id="modal">
